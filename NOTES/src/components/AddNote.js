08534092b686e071e82b343f5d66c19e04c515ff
Touisse <yassin.touisd@gmail.com>
2023-02-07 15:17:34 +0100

    notes app

---
+++ NOTES/src/components/AddNote.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+
+const AddNote = ({ handleAddNote }) => {
+  const [noteText, setNoteText] = useState("");
+  const characterLimit = 200;
+
+  const handleChange = (e) => {
+    if (characterLimit - e.target.value.length >= 0) {
+      setNoteText(e.target.value);
+    }
+  };
+  const handleClick = () => {
+    if (noteText.trim().length > 0) {
+      handleAddNote(noteText);
+      setNoteText("");
+    }
+  };
+  return (
+    <div className="note new">
+      <textarea
+        rows="8"
+        cols="10"
+        placeholder="Type to add  a note ..."
+        onChange={handleChange}
+      ></textarea>
+      <div className="note-footer">
+        <small>{characterLimit - noteText.length} Remaining</small>
+        <button className="save" onClick={handleClick}>
+          Save
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default AddNote;
---
+++ NOTES/src/components/Search.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { MdSearch } from "react-icons/md";
+
+const Search = ({ handleSearchNote }) => {
+  return (
+    <div className="search">
+      <MdSearch className="search-icon" size="1.3em" />
+      <input
+        onChange={(e) => handleSearchNote(e.target.value)}
+        type="text"
+        placeholder="Type to Search ..."
+      />
+    </div>
+  );
+};
+
+export default Search;
